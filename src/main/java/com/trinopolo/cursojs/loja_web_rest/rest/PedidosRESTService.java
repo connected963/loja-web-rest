9182432ebf868f30f96e3244eac535497361473c
Diego Keller <keller.diego@gmail.com>
2016-06-23 17:45:13 -0300

    Busca de pedido pelo id.

--- src/main/java/com/trinopolo/cursojs/loja_web_rest/rest/PedidosRESTService.java
+++ src/main/java/com/trinopolo/cursojs/loja_web_rest/rest/PedidosRESTService.java
@@ -16,27 +16,26 @@
  */
 package com.trinopolo.cursojs.loja_web_rest.rest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.trinopolo.cursojs.loja_web_rest.model.Pedido;
 import com.trinopolo.cursojs.loja_web_rest.service.LojaService;
 
-@Path("/pedido")
+@Path("/pedidos")
 @RequestScoped
-public class PedidoRESTService {
+public class PedidosRESTService {
 
 	@Inject
 	EntityManager em;
@@ -44,27 +43,19 @@ public class PedidoRESTService {
 	@Inject
 	LojaService lojaService;
 
-	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response salvar(Pedido pedido) {
-		try {
-			lojaService.salvarPedido(pedido);
-			return Response.status(Response.Status.OK).build();
-		} catch (Exception e) {
-			e.printStackTrace();
-			Map<String, String> responseObj = new HashMap<>();
-			responseObj.put("error", "Erro ao salvar o pedido: " + e.getMessage());
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(responseObj).build();
-		}
-	}
-
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("/{id}")
-	public Pedido buscar(@PathParam("id") Integer id) {
-		return em.find(Pedido.class, id);
+	@Path("/{usuarioId}")
+	public List<Pedido> buscar(@PathParam("usuarioId") Integer usuarioId) {
+		try {
+			TypedQuery<Pedido> query = em.createQuery("select e from Pedido as e where e.usuario.id = :id", Pedido.class);
+			query.setParameter("id", usuarioId);
+			return query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new LinkedList<>();
+		}
 	}
 
 }
